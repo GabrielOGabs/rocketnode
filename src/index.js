4552e0682697b1ff6999cf014e446679794d8aae
Gabriel de Paula Santos <gabriel.santos@outlook.com>
2022-03-20 02:47:30 -0400

    Added first routes and verbs to configure and test Insomnia app

--- src/index.js
+++ src/index.js
@@ -2,8 +2,32 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({"message": "Hello World"});
+app.use(express.json());
+
+app.get("/courses", (request, response) => {
+    return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.get("/courses/:id", (request, response) => {
+    return response.json(["Curso 1"]);
+});
+
+app.post("/courses", (request, response) => {
+    const params = request.body;
+    console.log(params);
+    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+    return response.json(["Curso 11", "Curso 22", "Curso 33"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(["Curso 1", "Curso 22", "Curso 3"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(["Curso 1", "Curso 3"]);
 });
 
 app.listen(3333);
